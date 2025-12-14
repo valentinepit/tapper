b6ebf5ae2269fc6af9dedbb7b9ed43855ca91d7d
valentin <piterans.v@gmail.com>
2025-12-14 16:01:24 +0500

    readme and starter

--- wplan/dist/wplan_starter.sh
+++ wplan/dist/wplan_starter.sh
@@ -4,7 +4,7 @@ export WPLAN_LOGIN="YOUR LOGIN"
 export WPLAN_PASS="YOUR PASSWORD"
 echo "=== Запуск wplan в $(date) ===" >> ~/wplan_cron.log
 
-/Users/valentin/wplan.sh 2>&1 | tee -a ~/wplan_cron.log
+/YOUR PATH/wplan.sh 2>&1 | tee -a ~/wplan_cron.log
 
 echo "=== Завершено с кодом $? ===" >> ~/wplan_cron.log
 EOF
